f296d16e4d6cc2ef2d7a9c689e662f76471ad4d0
Patrick Loonstra <patrick.loonstra@triggi.com>
2016-10-21 21:58:21 +0200

    opdracht 2 is af

---
+++ docs/02/assets/js/functions.js
@@ -0,0 +1,7 @@
+
+
+  console.log("Hello console!);")
+
+  function myFunction() {
+      alert("Hé Tom en Sythe. Na heel wat speuren en probeeren heb ik het voor elkaar. Ik heb gebruik gemaakt van een template voor de basis structuur. En begin de samenhang van de verschillende bestanden in te zien. Op naar opdracht 3!");
+  }
